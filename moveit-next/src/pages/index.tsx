c54fbc7f7979e268769cd15055cca1160e7c93f3
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-02-25 21:18:31 -0300

    create CompletedChallenges component

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head  from 'next/head';
+import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
@@ -12,6 +13,7 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
         </div>
 
         <div>
